af4dd05e0f11e19308765ac894b22f3e371b4e67
Your GitHub Username <cheathekm@gmail.com>
2026-02-08 11:52:20 +0700

    update icons

--- lib/icons/categories/app/index.ts
+++ lib/icons/categories/app/index.ts
@@ -2,3 +2,4 @@ export { AppleNews } from "./applenews";
 export { Telegram } from "./telegram";
 export { LinkedIn } from "./linkedin";
 export { Facebook } from "./facebook";
+export { Messenger } from "./messenger";
